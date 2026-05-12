5cabd415e442ee51431745884a65f90d0ef55429
Olly Butters <4135637+OllyButters@users.noreply.github.com>
2026-05-12 22:23:22 +0100

    Refactor HTML generation in build_packages_page.py to group packages by status and improve readability

--- web_pages/build_packages_page.py
+++ web_pages/build_packages_page.py
@@ -40,39 +40,77 @@ def main(csv_file_path, html_file_path):
             html_file.write('<th>GitHub owner</th>')
             html_file.write('</tr>')
 
+            production_rows = ""
+            development_rows = ""
+            retired_rows = ""
+            unknown_rows = ""
+
             row_count = 0
             for row in reader:
                 print(row)
                 row_count += 1
-                html_file.write('<tr>')
-                html_file.write('<td>' + str(row_count) + '</td>') # Row number
+
+                this_row = '<tr>'
+                #html_file.write('<tr>')
+                #html_file.write('<td>' + str(row_count) + '</td>') # Row number
+                this_row += '<td>' + str(row_count) + '</td>' # Row number
 
                 # Add GitHub link if available, otherwise just the name
                 if len(row[5]) > 0:
-                    html_file.write('<td class="left"><a href="' + row[5] + '" target="blank">' + row[0] + '</a></td>') # GH repository link with name
+                    #html_file.write('<td class="left"><a href="' + row[5] + '" target="blank">' + row[0] + '</a></td>') # GH repository link with name
+                    this_row += '<td class="left"><a href="' + row[5] + '" target="blank">' + row[0] + '</a></td>' # GH repository link with name
                 else:
-                    html_file.write('<td class="left">' + row[0] + '</td>')
+                    #html_file.write('<td class="left">' + row[0] + '</td>')
+                    this_row += '<td class="left">' + row[0] + '</td>'
 
                 # Description
                 if len(row[1]) > 0:
-                    html_file.write('<td class="left">' + row[1] + '</td>') # Description
+                    #html_file.write('<td class="left">' + row[1] + '</td>') # Description
+                    this_row += '<td class="left">' + row[1] + '</td>' # Description
                 else:
-                    html_file.write('<td></td>')
+                    #html_file.write('<td></td>')
+                    this_row += '<td></td>'
 
                 if len(row[2]) > 0:
-                    html_file.write('<td><a href="' + row[2] + '" target="blank">' + row[3] + '</a></td>') # CRAN link
+                    #html_file.write('<td><a href="' + row[2] + '" target="blank">' + row[3] + '</a></td>') # CRAN link
+                    this_row += '<td><a href="' + row[2] + '" target="blank">' + row[3] + '</a></td>' # CRAN link
                 else:
-                    html_file.write('<td></td>')
+                    #html_file.write('<td></td>')
+                    this_row += '<td></td>'
 
 
                 # html_file.write('<td>' + row[3] + '</td>') # CRAN version
-                html_file.write('<td>' + row[4] + '</td>') # CRAN license
-                html_file.write('<td>' + row[6] + '</td>') # GH last update
-                html_file.write('<td>' + row[7] + '</td>') # GH version
-                html_file.write('<td>' + row[8] + '</td>') # GH license
-                html_file.write('<td class="left">' + row[9] + '</td>') # GH owner
-                html_file.write('</tr>\n')
+                #html_file.write('<td>' + row[4] + '</td>') # CRAN license
+                this_row += '<td>' + row[4] + '</td>'
+                #html_file.write('<td>' + row[6] + '</td>') # GH last update
+                this_row += '<td>' + row[6] + '</td>'
+                #html_file.write('<td>' + row[7] + '</td>') # GH version
+                this_row += '<td>' + row[7] + '</td>'
+                #html_file.write('<td>' + row[8] + '</td>') # GH license
+                this_row += '<td>' + row[8] + '</td>'
+                #html_file.write('<td class="left">' + row[9] + '</td>') # GH owner
+                this_row += '<td class="left">' + row[9] + '</td>'
+                #html_file.write('</tr>\n')
+                this_row += '</tr>\n'
 
+                if row[10].strip() == 'production':
+                    production_rows += this_row
+                elif row[10].strip() == 'development':
+                    development_rows += this_row
+                elif row[10].strip() == 'retired':
+                    retired_rows += this_row
+                else:
+                    unknown_rows += this_row
+
+            # Build the table with the rows grouped by status
+            html_file.write('<tr><td colspan="9">Production</td></tr>')
+            html_file.write(production_rows)
+            html_file.write('<tr><td colspan="9">Development</td></tr>')
+            html_file.write(development_rows)
+            html_file.write('<tr><td colspan="9">Retired</td></tr>')
+            html_file.write(retired_rows)
+            html_file.write('<tr><td colspan="9">Unknown</td></tr>')
+            html_file.write(unknown_rows)
             html_file.write('</table>')
 
         html_file.write('Generated on ' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
