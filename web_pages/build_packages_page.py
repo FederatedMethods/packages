7d55b83005f07b71145c113972301bb9c70ef6c6
Olly Butters <4135637+OllyButters@users.noreply.github.com>
2026-05-12 12:31:06 +0100

    Update package_list.csv and HTML generation to enhance package information display

--- web_pages/build_packages_page.py
+++ web_pages/build_packages_page.py
@@ -28,7 +28,17 @@ def main(csv_file_path, html_file_path):
 
             html_file.write('<h1>DataSHIELD packages</h1>\n')
             html_file.write('<table border="1">\n')
-            html_file.write('<tr><td></td>' + ''.join(f'<th>{header}</th>' for header in headers) + '</tr>\n')
+
+            html_file.write('<tr><td></td>')
+            html_file.write('<th>Name</th>')
+            html_file.write('<th>Description</th>')
+            html_file.write('<th>CRAN version</th>')
+            html_file.write('<th>CRAN license</th>')
+            html_file.write('<th>GitHub last update</th>')
+            html_file.write('<th>GitHub version</th>')
+            html_file.write('<th>GitHub license</th>')
+            html_file.write('<th>GitHub owner</th>')
+            html_file.write('</tr>')
 
             row_count = 0
             for row in reader:
@@ -36,23 +46,31 @@ def main(csv_file_path, html_file_path):
                 row_count += 1
                 html_file.write('<tr>')
                 html_file.write('<td>' + str(row_count) + '</td>') # Row number
-                #html_file.write('<td>' + row[0] + '</td>') # Name
+
+                # Add GitHub link if available, otherwise just the name
                 if len(row[5]) > 0:
-                    html_file.write('<td><a href="' + row[5] + '" target="blank">' + row[0] + '</a></td>') # GH repository link with name
+                    html_file.write('<td class="left"><a href="' + row[5] + '" target="blank">' + row[0] + '</a></td>') # GH repository link with name
                 else:
-                    html_file.write('<td>' + row[0] + '</td>')
-                html_file.write('<td>' + row[1] + '</td>') # Description
-                if len(row[2]) > 0:
-                    html_file.write('<td><a href="' + row[2] + '" target="blank">Link</a></td>') # CRAN link
+                    html_file.write('<td class="left">' + row[0] + '</td>')
+
+                # Description
+                if len(row[1]) > 0:
+                    html_file.write('<td class="left">' + row[1] + '</td>') # Description
                 else:
                     html_file.write('<td></td>')
-                html_file.write('<td>' + row[3] + '</td>') # CRAN version
+
+                if len(row[2]) > 0:
+                    html_file.write('<td><a href="' + row[2] + '" target="blank">' + row[3] + '</a></td>') # CRAN link
+                else:
+                    html_file.write('<td></td>')
+
+
+                # html_file.write('<td>' + row[3] + '</td>') # CRAN version
                 html_file.write('<td>' + row[4] + '</td>') # CRAN license
-               
                 html_file.write('<td>' + row[6] + '</td>') # GH last update
                 html_file.write('<td>' + row[7] + '</td>') # GH version
                 html_file.write('<td>' + row[8] + '</td>') # GH license
-                html_file.write('<td>' + row[9] + '</td>') # GH owner
+                html_file.write('<td class="left">' + row[9] + '</td>') # GH owner
                 html_file.write('</tr>\n')
 
             html_file.write('</table>')
